9a1d19106a39cccc360b2f7d4e076786cedc1300
Scott <scott@boatrockr.online>
2024-10-01 18:00:43 +1000

    search func

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { config } from './config.js';
 import Database from './database.js';
+import data from './testdata.js';
 
 // Import App routes
 import delegate from './delegate.js';
@@ -15,6 +16,19 @@ const app = express();
 app.use('/api-docs', openapi);
 app.use('/delegates', delegate);
 app.use('/cities', city);
+
+//GET Search
+app.use('search/', async (req, res) => {
+  try{
+      const searchParams = req.query
+      console.log(searchParams)
+      const delegates = await data.find(searchParams)
+      if(!delegates) throw Error('Error, No Delegates Found...!')
+      res.status(200).json(delegates)
+  }catch(err){
+      res.status(400).json({msg:err})
+  }
+})
 
 app.use('*', (_, res) => {
   res.redirect('/api-docs');
